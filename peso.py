988179cc59a5e22413c029df61cda228a88ce252
santiagoer222 <133126093+santiagoer222@users.noreply.github.com>
2023-05-21 15:48:27 -0500

    Update peso.py

--- peso.py
+++ peso.py
@@ -1,16 +1,16 @@
 def calcular_peso_en_otro_planeta(peso_tierra, gravedad_planeta):
-    peso_otro_planeta = peso_tierra * gravedad_planeta / 9.8
+    peso_otro_planeta = masa_tierra * gravedad_planeta / 9.8
     return peso_otro_planeta
 
 # Peso en la Tierra (en kilogramos)
-peso_tierra = float(input("Ingresa el peso en la Tierra (en kg): "))
+peso_tierra = float(input("Ingresa el peso en la Tierra (en N): "))
 
 # Calcular peso en la Luna
 gravedad_luna = 1.622  # gravedad en la Luna en m/s^2
 peso_luna = calcular_peso_en_otro_planeta(peso_tierra, gravedad_luna)
-print("El peso en la Luna es:", peso_luna, "kg")
+print("El peso en la Luna es:", peso_luna, "N")
 
 # Calcular peso en Marte
 gravedad_marte = 3.71  # gravedad en Marte en m/s^2
 peso_marte = calcular_peso_en_otro_planeta(peso_tierra, gravedad_marte)
-print("El peso en Marte es:", peso_marte, "kg")
+print("El peso en Marte es:", peso_marte, "N")
